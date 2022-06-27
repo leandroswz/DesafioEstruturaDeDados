2cd9b8911e0c847b2b6b520bccdb6f3a04fe2283
Leandro Costa <leandrin.costta@gmail.com>
2022-06-27 13:08:58 -0300

    Desafio Estrutura de Dados

---
+++ script.js
@@ -0,0 +1,25 @@
+ // Crie um array que receba 5 itens e exiba no console.
+let nomes = ["Amanda","Leo","Patricia","Luiza","Theo"]
+console.log(nomes)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+nomes.unshift("Leandro")
+console.log(nomes)
+
+// Utilize um método para remover o último nome do array.
+nomes.pop()
+console.log(nomes)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+nomes.push("Matheus","Luciana")
+console.log(nomes)
+
+// Utilize um método para remover o primeiro nome do array.
+nomes.shift()
+console.log(nomes)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+let numbers = [7,5,6,3,8,9,2,1,4]
+console.log(numbers)
+console.log("Ordem numérica: ",numbers.sort())
